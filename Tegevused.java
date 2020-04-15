99c79de16c40a86db26cbc936f0d461c254e6492
MartinPuskin <martin.puskin@gmail.com>
2020-04-16 00:20:48 +0300

    Esimene aken + algarvukontroll
    Muutsin BorderPane abiga esimese akna kujundust, eraldasin tehted ringi järgi.
    Lisasin TextFieldi, mis kontrollib, kas sisend on algarv (või üldse arv). Vastasel juhul annab vastava sõnumi.

--- Tegevused.java
+++ Tegevused.java
@@ -1,30 +1,66 @@
-package oop;
-
+package projekt;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Tegevused extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public boolean onAlgarv(int arv){
+        if (arv < 2){
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(arv) ; i++) {
+            if(arv % i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public void start(Stage peaLava) {
-        VBox juur= new VBox(5);
+        BorderPane juur = new BorderPane();
+        VBox vbox1 = new VBox(5);
+        VBox vbox2 = new VBox(5);
+        VBox vbox3 = new VBox(10);
 
-        Label tekst = new Label("Millist tehet soovid teha?");
-        Label vastus= new Label();
+        Label lab1 = new Label("Millist tehet soovid teha?");
+        Label lab2 = new Label("Tehted jäägiklassi-\nkorpuses:");
+        Label lab3 = new Label("Tehted polünoomi-\nringis");
+        Label lab4 = new Label("Sisesta algarvuline moodul:");
+        Label lab5 = new Label();
+        lab5.setVisible(false);
+        TextField tf = new TextField();
 
+        //Jäägiklassi tehted
         RadioButton raadio1=new RadioButton("1. Liida");
         RadioButton raadio2= new RadioButton("2. Lahuta");
         RadioButton raadio3= new RadioButton("3. Korruta");
         RadioButton raadio4= new RadioButton("4. Jaga");
         RadioButton raadio5= new RadioButton("5. Astenda");
-        RadioButton raadio6= new RadioButton("6. Leia SÜT");
-        RadioButton raadio7= new RadioButton("7. Leia VÜK");
-        RadioButton raadio8= new RadioButton("8. Astenda");
+        RadioButton raadio6= new RadioButton("6. Pöördelement");
+
+        //polünoomide klassi tehted
+        RadioButton raadio7= new RadioButton("1. Liida");
+        RadioButton raadio8= new RadioButton("2. Lahuta");
+        RadioButton raadio9= new RadioButton("3. Korruta");
+        RadioButton raadio10= new RadioButton("4. Jaga");
+        RadioButton raadio11= new RadioButton("5. Astenda");
+        RadioButton raadio12= new RadioButton("6. SÜT");
+        RadioButton raadio13= new RadioButton("7. VÜK");
+
 
         ToggleGroup küsimus= new ToggleGroup();
         raadio1.setToggleGroup(küsimus);
@@ -35,28 +71,69 @@ public class Tegevused extends Application{
         raadio6.setToggleGroup(küsimus);
         raadio7.setToggleGroup(küsimus);
         raadio8.setToggleGroup(küsimus);
+        raadio9.setToggleGroup(küsimus);
+        raadio10.setToggleGroup(küsimus);
+        raadio11.setToggleGroup(küsimus);
+        raadio12.setToggleGroup(küsimus);
+        raadio13.setToggleGroup(küsimus);
 
-        Button nupp= new Button("Esita");
 
-        /*nupp.setOnAction(e ->
+        Button nupp = new Button("Esita");
+
+        nupp.setOnAction(e ->
         {
-            if (raadio2.isSelected()) {
-                nupp.setOnMouseClicked(me -> vastus.setText("Õige vastus"));
-            } else {
-                nupp.setOnMouseClicked(me -> vastus.setText("Vale vastus"));
+            try {
+                int arv = Integer.parseInt(tf.getCharacters().toString());
+                if (onAlgarv(arv)) {
+                    lab5.setVisible(false);
+                    if (raadio1.isSelected()) {
+
+                    } else if (raadio2.isSelected()) {
+
+                    } else if (raadio2.isSelected()) {
+
+                    } else if (raadio3.isSelected()) {
+
+                    } else if (raadio4.isSelected()) {
+
+                    } else if (raadio5.isSelected()) {
+
+                    } else if (raadio6.isSelected()) {
+
+                    } else if (raadio7.isSelected()) {
+
+                    } else if (raadio8.isSelected()) {
+
+                    } else if (raadio9.isSelected()) {
+
+                    } else if (raadio10.isSelected()) {
+
+                    } else if (raadio11.isSelected()) {
+
+                    } else if (raadio12.isSelected()) {
+
+                    } else if (raadio13.isSelected()) {
+
+                    }
+                }else{
+                    lab5.setText("Moodul peab olema algarv!");
+                    lab5.setVisible(true);
+                }
+            }catch (NumberFormatException e1){
+                lab5.setText("Sisestage arv!");
+                lab5.setVisible(true);
             }
-        });*/
+        });
 
-        juur.getChildren().addAll(tekst, raadio1, raadio2, raadio3,raadio4,raadio5,raadio6,raadio7, nupp, vastus);
-
-        Scene stseen1 = new Scene(juur, 300, 300, Color.SNOW);
+        vbox1.getChildren().addAll(lab2, raadio1, raadio2, raadio3, raadio4, raadio5, raadio6);
+        vbox2.getChildren().addAll(lab3, raadio7, raadio8, raadio9, raadio10, raadio11, raadio12, raadio13);
+        vbox3.getChildren().addAll(lab1, lab4, tf, nupp, lab5);
+        juur.setLeft(vbox1);
+        juur.setRight(vbox2);
+        juur.setCenter(vbox3);
+        Scene stseen1 = new Scene(juur, 475, 300, Color.SNOW);
         peaLava.setTitle("Tegevused");
-        peaLava.setResizable(false);
         peaLava.setScene(stseen1);
         peaLava.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
